f0c14d281243b55b1a1ac7defa8afdb9228a3266
Antooon32 <antonskripka11@gmail.com>
2026-06-29 11:39:24 +0300

    hw

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 import Handlebars from "handlebars";
 import сardTemplate from "./template.hbs?raw";
 
-const data = { 
-  title: 'Create content from Template and Data!', 
-  items: ['LoDash', 'NanoID', 'PNotify', 'Chart.js', 'BasicLightbox', 'Handlebars'] 
-};
+export const products = [
+  {
+    id: 1,
+    name: 'Laptop',
+    price: 1500,
+    description: 'A high-performance laptop for all your needs.',
+  },
+  {
+    id: 2,
+    name: 'Smartphone',
+    price: 700,
+    description: 'A modern smartphone with an excellent camera.',
+  },
+  {
+    id: 3,
+    name: 'Headphones',
+    price: 200,
+    description: 'Noise-cancelling headphones for better focus.',
+  },
+];
+
 
 const template = Handlebars.compile(сardTemplate);
 console.log(template);
-const markup = template(data);
+const markup = template(products);
 
 const container = document.querySelector('#menu-container');    
 container.innerHTML = markup;
